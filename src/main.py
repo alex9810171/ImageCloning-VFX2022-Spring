b2899a7adfc25742d8d2304ffbe9e7525bffccb4
Alex_Hsu <alex9810171@gmail.com>
2022-06-13 00:30:47 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
-from base64 import b64encode
 import numpy as np
-import random
-import string
 import cv2
 from flask import Flask, render_template, request, Response, jsonify
-import requests
 from src.cloning import cloning_api_for_web
+from src.upload_image import upload_to_imgur
 
 config = {
     'background_image_path': r'test/ice-coast.png',
@@ -17,26 +14,6 @@ config = {
     'kernel': np.ones((3, 3), np.uint8),
     'object_threshold': 500
 }
-
-CLIENT_ID = '47a12b43b020d51'
-def upload_to_imgur(img):
-    # get a random name string for the image
-    name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
-    # encode the image to base64
-    ret, img_encoded = cv2.imencode('.jpg', img)
-    img_encoded = b64encode(img_encoded)
-    # upload the image to imgur
-    response = requests.post('https://api.imgur.com/3/image',
-                                headers={'Authorization': 'Client-ID ' + CLIENT_ID},
-                                data={
-                                    'image': img_encoded,
-                                    'type': 'base64',
-                                    'name': f'img_encoded.jpg',
-                                    'title': name}).json()
-    if response['status'] == 200:
-        return response['data']['link']
-    else:
-        return None
 
 app = Flask(__name__)
 bkg_image = cv2.imread(config['background_image_path'])
@@ -60,14 +37,11 @@ def cloning_api():
     if img.shape[2] != 3:
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     result = cloning_api_for_web(bkg_image, img, int(request.form.get('x-axis', 200)), int(request.form.get('y-axis', 200)))
-    # print(result)
     result_link = upload_to_imgur(result)
     if result_link is None:
         return Response(status=500)
     else:
         return jsonify({'result': result_link})
-
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
---
+++ src/upload_image.py
@@ -0,0 +1,25 @@
+from base64 import b64encode
+import random
+import string
+import requests
+import cv2
+
+CLIENT_ID = '47a12b43b020d51'
+def upload_to_imgur(img):
+    # get a random name string for the image
+    name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+    # encode the image to base64
+    ret, img_encoded = cv2.imencode('.jpg', img)
+    img_encoded = b64encode(img_encoded)
+    # upload the image to imgur
+    response = requests.post('https://api.imgur.com/3/image',
+                                headers={'Authorization': 'Client-ID ' + CLIENT_ID},
+                                data={
+                                    'image': img_encoded,
+                                    'type': 'base64',
+                                    'name': f'img_encoded.jpg',
+                                    'title': name}).json()
+    if response['status'] == 200:
+        return response['data']['link']
+    else:
+        return None
